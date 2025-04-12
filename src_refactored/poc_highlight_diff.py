80b91ba2431bca3d50eeccf3efb53e9149323440
Ashok Maurya <ashok.samar1@gmail.com>
2025-04-12 19:52:06 +0530

    adx,adls,highlight_diff con

--- src_refactored/poc_highlight_diff.py
+++ src_refactored/poc_highlight_diff.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import numpy as np
 
 def highlight_diff(self, row):
-    """Highlights mismatched cells."""
     styles = [''] * len(row)
     start_idx = len(self.config['comparison_keys'])
 
@@ -9,13 +9,16 @@ def highlight_diff(self, row):
         val1 = row.iloc[i]
         val2 = row.iloc[i + 1]
 
-        # Handle NaN-safe comparison
         if pd.isna(val1) and pd.isna(val2):
             continue
-        if not pd.isna(val1) and not pd.isna(val2) and val1 != val2:
-            styles[i] = 'background-color: #FF6347'
-            styles[i + 1] = 'background-color: #FF6347'
-        elif pd.isna(val1) != pd.isna(val2):  # One is NaN, other is not
+        elif pd.isna(val1) or pd.isna(val2):
+            mismatch = True
+        elif isinstance(val1, (int, float, np.number)) and isinstance(val2, (int, float, np.number)):
+            mismatch = not np.isclose(val1, val2, equal_nan=True)
+        else:
+            mismatch = str(val1) != str(val2)
+
+        if mismatch:
             styles[i] = 'background-color: #FF6347'
             styles[i + 1] = 'background-color: #FF6347'
 
